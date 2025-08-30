203a35fa6a8d4a14b3026c261beac2e834610409
andrespadeto <andrespadeto@gmail.com>
2025-08-30 19:19:27 -0300

    feedbacks

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         <main className="max-w-full flex justify-between items-center pt-18">
             <div className='w-[560px]'>
                 <h1 className='text-5xl font-bold mb-6 leading-tight'>Controle suas finanças em apenas 15 minutos por semana e <span className='h1-b'>tenha dinheiro</span> de sobra todo mês! Tudo de forma <span className='h1-b'>automatizada e simplificada</span>.</h1>
-                <p className='text-lg mb-6'>Não importa quanto você ganha, com um bom planejamento e a <strong>ferramenta ideal</strong> é possível fazer sobrar dinheiro e investir no que realmente importa para você.</p>
+                <p className='text-lg mb-11'>Não importa quanto você ganha, com um bom planejamento e a <strong>ferramenta ideal</strong> é possível fazer sobrar dinheiro e investir no que realmente importa para você.</p>
                 <Button1 href="#" className='btn1-mpf'>Quero ter tranquilidade financeira</Button1>
             </div>
             <div className='w-[540px] pl-10'>
